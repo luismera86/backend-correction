4b91c0448d80de07e4877228f288b80f7ab8e80e
Luis Mera <luismera86@gmail.com>
2022-12-13 23:01:25 -0300

    Validación de id mongo agregado

--- src/models/delivery.model.ts
+++ src/models/delivery.model.ts
@@ -1,45 +1,45 @@
-import { model, Schema } from "mongoose";
-import { Delivery } from "./interfaces";
+import { model, Schema } from 'mongoose'
+import { Delivery } from './interfaces'
 
 const deliverySchema = new Schema<Delivery>({
-    commentOne: {
-        type: String
+  commentOne: {
+    type: String,
+  },
+  commentTow: {
+    type: String,
+  },
+  commentThree: {
+    type: String,
+  },
+  commentFour: {
+    type: String,
+  },
+  commentFive: {
+    type: String,
+  },
+  feedback: {
+    type: String,
+  },
+  preDeliveryNumber: {
+    type: Number,
+  },
+  student: {
+    name: {
+      type: String,
     },
-    commentTow: {
-        type: String
+    tutor: {
+      type: String,
     },
-    commentThree: {
-        type: String
+    commission: {
+      type: Number,
     },
-    commentFour: {
-        type: String
+    course: {
+      type: String,
     },
-    commentFive: {
-        type: String
-    },
-    feedback:{
-        type: String
-    },
-    preDeliveryNumber: {
-        type: Number
-    },
-    student: {
-        name: {
-            type: String
-        },
-        tutor: {
-            type: String
-        },
-        commission: {
-            type: Number
-        },
-        course: {
-            type: String
-        },
-    },
-    result: {
-        type: String
-    },
+  },
+  result: {
+    type: String,
+  },
 })
 
 const Delivery = model<Delivery>('Delivery', deliverySchema)
